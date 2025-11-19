77556bcfb4c6abfd0e7a4f3371f79824d5a525ba
kerillio <kerillkrutoi@gmail.com>
2025-11-19 17:27:51 +0300

    small fix#2

--- calc.py
+++ calc.py
@@ -1,25 +1,6 @@
 import cmath
 import array
 
-userString = str(input("Input string: "))
-strArray = userString.split(' ')
-
-print(strArray)
-
-char = strArray[1]
-num1 = int(strArray[0])
-num2 = int(strArray[2])
-
-
-if char == '+':
-    print('result: ', num1+num2)
-elif char == '-':
-    print('result: ', num1-num2)
-elif char == '*':
-    print('result: ', num1*num2)
-elif char == '/':
-    print('result: ', num1+num2)
-
 #A simple calculator
 class Calculator:
   #empty constructor
